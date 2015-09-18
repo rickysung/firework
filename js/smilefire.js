527449b8e6a2608f530d2d80385947676c50c83b
root <root@ip-172-31-19-94.ap-southeast-1.compute.internal>
2015-09-18 11:52:35 +0000

    add smilefire and doubleexploder

--- js/smilefire.js
+++ js/smilefire.js
@@ -1,8 +1,7 @@
-var gravity = new Vector3D(1, new Point3D(0,1,0));
-function DoubleFire() 
+function SmileFire()
 {
-	this.firePoints = new Array(5000);
-	this.fireVectors = new Array(5000);
+	this.firePoints = new Array(4000);
+	this.fireVectors = new Array(4000);
 	this.fn=0;
 	this.x=0;
 	this.y=0;
@@ -11,26 +10,48 @@ function DoubleFire()
 	this.explodeduration=0;
 	var tick;
 	this.IsExplode=false;
+	this.IsDone = false;
+	var gravity = new Vector3D(1, new Point3D(0,1,0));
 	this.FireInit = function(point, div, vel, color, exde, exdu)
-	{	
-		var cof = 0.3;
-		var colorselector = Math.random();
+	{
+			var cof = 0.1;
 		for(var i= 0; i<2*div ; i++)
 		{
-			for(var j= 1 ; j<div ; j++){
-				var ang = (Math.PI+getRandom(-cof,cof))*j/div;
-				var tx = Math.cos(ang);
-				var ty = -Math.sin(ang);
-				var tz = 0;
-				ang = (Math.PI+getRandom(-cof,cof))*i/div;
-				var x = tx;
-				var y = Math.cos(ang)*ty;
-				var z = -Math.sin(ang)*ty;
-				this.fireVectors[this.fn] = new Vector3D(vel, new Point3D(x,y,z));
-				this.firePoints[this.fn] = new Point3D(x,y,z);
-				this.fn++;
-			}
+			var z = 0;
+			ang = (Math.PI+getRandom(-cof,cof))*i/div;
+			var x = Math.cos(ang);
+			var y = -Math.sin(ang);
+			this.fireVectors[this.fn] = new Vector3D(vel, new Point3D(x,y,z));
+			this.firePoints[this.fn] = new Point3D(x,y,z);
+			this.firePoints[this.fn].color = color;
+			this.fn++;
 		}
+		for(var i=0 ; i<div ; i++)
+		{	
+			var z =0;
+			ang = -(Math.PI + getRandom(-cof,cof))*i/div;
+			var x = Math.cos(ang);
+			var y = -Math.sin(ang);
+			this.fireVectors[this.fn] = new Vector3D(vel/2, new Point3D(x,y,z));
+			this.firePoints[this.fn] = new Point3D(x,y,z);
+			this.firePoints[this.fn].color = "red";
+			this.fn++;
+		}
+		ang = Math.PI/4;
+		var x = Math.cos(ang);
+		var y = -Math.sin(ang);
+		this.fireVectors[this.fn] = new Vector3D(vel/2, new Point3D(x,y,0));
+		this.firePoints[this.fn] = new Point3D(x,y,0);
+		this.firePoints[this.fn].color = "red";
+		this.fn++;
+		ang = Math.PI*3/4;
+		var x = Math.cos(ang);
+		var y = -Math.sin(ang);
+		this.fireVectors[this.fn] = new Vector3D(vel/2, new Point3D(x,y,0));
+		this.firePoints[this.fn] = new Point3D(x,y,0);
+		this.firePoints[this.fn].color = "red";
+		this.fn++;
+
 		var w = getRandom(-90,90) * Math.PI/180;
 		for(var i=0 ; i<this.fn ; i++)
 		{
@@ -55,12 +76,12 @@ function DoubleFire()
 		this.x = point.X;
 		this.y = point.Y;
 		this.z = point.Z;
+
 		for(var i=0 ;  i<this.fn ; i++)
 		{
 			this.firePoints[i].X += this.x;
 			this.firePoints[i].Y += this.y;
 			this.firePoints[i].Z += this.z;
-			this.firePoints[i].color = color;
 		}
 		this.explodedelay = exde;
 		this.explodeduration = exdu;
@@ -68,7 +89,7 @@ function DoubleFire()
 		this.IsExplode = false;
 		tick=0;
 	}
-	this.progress = function()
+	this.progress = function(c)
 	{
 		tick++;
 		if(this.IsExplode == false)
@@ -82,15 +103,18 @@ function DoubleFire()
 			{
 				for(var i=0 ; i<this.fn ; i++)
 				{
-					this.firePoints[i].Add(this.fireVectors[i]);
-					this.firePoints[i].Add(gravity);
+					this.firePoints[i].Add(c, this.fireVectors[i]);
+					this.firePoints[i].Add(c, gravity);
 					this.fireVectors[i].mag/=1.01;
 				}
 				if(this.explodedelay+this.explodeduration - tick <=40)
 				{
 					this.opac -= 0.025;
 					if(this.opac < 0.05)
+					{
 						this.opac = 0;
+						IsDone = true;
+					}
 				}
 			}
 		}
